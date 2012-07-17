eb89fc464053c0fe159da69f8601fd93b26f1b0e
MitzaCeusan <mitza.ceusan@gmail.com>
2012-07-17 21:33:31 +0300

    try

---
+++ index.php
@@ -0,0 +1,124 @@
+<head>
+  <link href='http://yui.yahooapis.com/3.5.1/build/cssreset/cssreset-min.css' media='screen' rel='stylesheet' type='text/css' />
+  <link href='stylesheets/css/main.css' media='screen' rel='stylesheet' type='text/css' />
+  <script src='js/jquery-1.7.2.min.js'></script>
+  <script src='js/waypoints.min.js'></script>
+  <script src='js/jquery.scrollTo-1.4.2-min.js'></script>
+  <script src='js/jquery.easing.1.3.js'></script>
+  <script src='js/jquery.pitchdeck.js'></script>
+  <script src='js/main.js'></script>
+</head>
+<div class='deck'>
+  <div id='nav'>
+    <div class='table_container'>
+      <a class='nav toplink' href='#slide_1' title='1'></a>
+      <div class='nav_spacer'></div>
+      <a class='nav toplink' href='#slide_2' title='2'></a>
+      <div class='nav_spacer'></div>
+      <a class='nav toplink' href='#slide_3' title='3'></a>
+      <div class='nav_spacer'></div>
+      <a class='nav toplink' href='#slide_4' title='4'></a>
+      <div class='nav_spacer'></div>
+      <a class='nav toplink' href='#slide_5' title='5'></a>
+      <div class='nav_spacer'></div>
+      <a class='nav toplink' href='#slide_6' title='6'></a>
+      <div class='nav_spacer'></div>
+      <a class='nav toplink' href='#slide_7' title='7'></a>
+    </div>
+  </div>
+  <div class='slide blue_seafoam margin_20t' id='slide_1'>
+    <img class='cloud' src='images/complex-cloud-1-lg.png' style='left: -230px; top: 80px' />
+    <img class='cloud' src='images/simple-cloud-1-lg.png' style='right: -296px; top: 330px' />
+  </div>
+  <div class='slide_with_bleed green_forest'>
+    <div class='slide' id='slide_2'>
+      <div class='content_area_1'>
+        <div class='title_1 title_caption_style text_blue_green'>A caption style text to lead into some text...</div>
+        <div class='text_white text_1'>
+          This is a nice long paragraph of text with some bold text. Your buyer had removed much of the sales process, completing
+          <b>70%</b>
+          of their research
+          <b>BEFORE</b>
+          they ever contact a sales person.
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class='slide blue_1' id='slide_3'>
+    <div class='content_area_1'>
+      <div class='title_1 title_small text_blue_green'>If it's a small title it could look like this</div>
+      <div class='list_centered text_green_forest'>
+        <div class='list_item_1'>Here are some short bullet points with a little text</div>
+        <div class='list_item_1'>More emphasis on marketing metrics</div>
+        <div class='list_item_1'>Ads becoming less effective</div>
+        <div class='list_item_1'>An SEO landscape that is moving toward more relevancy</div>
+        <div class='list_item_1'>Empty social media strategies</div>
+      </div>
+    </div>
+  </div>
+  <div class='slide_with_bleed green_aqua'>
+    <div class='slide slide_short' id='slide_4'>
+      <div class='callout_awesome title_callout text_white'>If you want to emphasize a line or two of text, let's have an awesome callout</div>
+    </div>
+  </div>
+  <div class='slide blue_sky' id='slide_5'>
+    <div class='title_area_1'>
+      <div class='title_large text_gray_dark'>Also makes for a pretty good large title</div>
+      <div class='title_secondary text_orange_burnt text_right'>... yeah?</div>
+    </div>
+    <div class='image_area_1 width_300'>
+      <img src='images/tcm.png' />
+      <div class='caption_area text_green_forest'>
+        <div class='caption_title'>Caption for a graphic</div>
+        <div class='caption_text'>(pretend it's another graphic)</div>
+      </div>
+    </div>
+  </div>
+  <div class='slide_with_bleed gray'>
+    <div class='slide' id='slide_6'>
+      <div class='content_area_1'>
+        <table class='table_1'>
+          <td class='width_160'>
+            <div class='title_1 text_blue_green'>Fast, easy adoption of Kapost:</div>
+          </td>
+          <td>
+            <div class='text_green_forest'>
+              <div class='list_item_2'>
+                <b>Saas</b>
+                (Software-as-a-service)
+              </div>
+              <div class='list_item_2'>
+                <b>Easy to use,</b>
+                easy to deploy
+              </div>
+              <div class='list_item_2'>
+                <b>Quickly</b>
+                imports your current data & never holds your data hostage... ever.
+              </div>
+              <div class='list_item_2'>
+                <b>Plus</b>
+                you get a 30 day trial to kick the tires
+              </div>
+            </div>
+          </td>
+        </table>
+      </div>
+    </div>
+  </div>
+  <div class='slide white slide_short' id='slide_7'>
+    <div class='content_area_2'>
+      <table class='table_contact'>
+        <td class='text_amatic'>Contact</td>
+        <td>(kp logo)</td>
+        <td>
+          <p>
+            <a class='link_style_1' href='http://kapost.com'>Kapost.com</a>
+          </p>
+          <p>
+            <a class='link_style_1' href='mailto:sales@kapost.com'>sales@kapost.com</a>
+          </p>
+        </td>
+      </table>
+    </div>
+  </div>
+</div>
