50d9473c7e66bbf61cf99a108b3e96c36be049e1
MitzaCeusan <mitza.ceusan@gmail.com>
2012-07-25 22:42:28 +0300

    more updates

--- index.php
+++ index.php
@@ -32,52 +32,123 @@
       <div class='text_gray_light text_nixie padding_230t relative font_size_48 zindex_9'>Building and Running Your Own Content Marketing Machine</div>
       <div class='text_nixie relative zindex_9 text_gray_dark font_size_24 margin_60t'>- on -</div>
       <img class='zindex_9 relative' src='images/kapost-logo.png' />
-      <img class='cloud' data-stellar-horizontal-offset='120' data-stellar-ratio='0.6' data-stellar-vertical-offset='20' src='images/complex-cloud-1-lg.png' style='left: -220px; top: 0px' />
-      <img class='cloud' data-stellar-horizontal-offset='120' data-stellar-ratio='0.6' data-stellar-vertical-offset='40' src='images/simple-cloud-1-lg.png' style='' />
+      <img class='cloud' data-stellar-horizontal-offset='120' data-stellar-ratio='0.7' src='images/complex-cloud-1-lg.png' style='left: -220px; top: 0px' />
+      <!-- %img{:src => "images/simple-cloud-1-lg.png", :style => "right: -276px; top: 420px", :class => "cloud", :"data-stellar-ratio" => 1.3, :"data-stellar-horizontal-offset" => "-1220", :"data-stellar-vertical-offset" => "40"} -->
+      <img class='cloud' data-stellar-horizontal-offset='130' data-stellar-ratio='0.8' data-stellar-vertical-offset='40' src='images/simple-cloud-1-lg.png' style='' />
       <img class='cloud' src='images/simple-cloud-2-lg.png' style='left: -150px; top: 775px' />
     </div>
   </div>
-  <div class='slide height_885 slide_with_bleed' id='slide_2'>
+  <div class='slide height_868 slide_with_bleed' id='slide_2'>
     <div class='slide_container blue_seafoam height_100'></div>
-    <div class='green_forest zindex_9 relative'>
+    <div class='green_forest zindex_10 relative'>
       <div class='slide_container'>
-        <div class='title_1 title_caption_style text_gray_dark'>MARKETING MUST GENERATE LEADS & OPPORTUNITIES</div>
-        <div class='text_white text_1'>
+        <div class='title_1 title_caption_style text_gray_dark text_arvo'>MARKETING MUST GENERATE LEADS & OPPORTUNITIES</div>
+        <div class='text_white_dark text_1 margin_34t'>
           The Internet has put the Buyer in control.
         </div>
-        <div class='text_white text_1'>
-          <a href='http://www.google.com/url?q=http%3A%2F%2Fwww.eloqua.com%2Fgrande%2FGrande_Guide_To_Sales_Enablement.html&amp;sa=D&amp;sntz=1&amp;usg=AFQjCNEaiBO9eQ4WD-IOqYNHkrlGD4HA1Q' target='_blank'>
-            <b>70%</b>
-            of the Buying Cycle
-          </a>
-          &nbsp;is complete before Sales is contacted.
+        <div class='text_white_dark text_1'><a href='http://www.eloqua.com/grande/Grande_Guide_To_Sales_Enablement.html' target='_blank'><b class='font_size_48'>70%</b>
+          of the Buying Cycle</a>&nbsp;is complete
+          <br />
+          before Sales is contacted.
         </div>
-        <div class='text_white text_1'>
-          Marketing must generate leads and nurture them into opportunities.
+        <div class='text_white_dark text_1'>
+          Marketing must generate leads
+          <br />
+          and nurture them into opportunities.
         </div>
       </div>
     </div>
-    <div class='slide_container blue_seafoam height_240'>
+    <div class='slide_container blue_seafoam height_240 zindex_9'>
       <img class='cloud' src='images/complex-cloud-2-sm.png' style='left: -70px; top: -135px' />
     </div>
   </div>
   <div class='slide' id='slide_3'>
-    <div class='slide_container blue_seafoam text_center height_1000'>
-      <div class='content_area_1'>
-        <div class='title_1 title_small text_blue_green'>If it's a small title it could look like this</div>
-        <div class='list_centered text_green_forest'>
-          <div class='list_item_1'>Here are some short bullet points with a little text</div>
-          <div class='list_item_1'>More emphasis on marketing metrics</div>
-          <div class='list_item_1'>Ads becoming less effective</div>
-          <div class='list_item_1'>An SEO landscape that is moving toward more relevancy</div>
-          <div class='list_item_1'>Empty social media strategies</div>
-        </div>
+    <div class='slide_container blue_seafoam height_1000 padding_70t'>
+      <div class='margin_25t title_2 text_blue_3 text_arvo text_center'>
+        CONTENT MARKETING IS
+        <b>CRITICAL</b>
+        TO LEAD &amp; OPPORTUNITY GENERATION
       </div>
+      <div class='subtitle_1 margin_40t'>
+        LEAD GENERATION -
+      </div>
+      <table class='table_2'>
+        <td>
+          <div class='title_secondary text_blue_green'>Ads don't work</div>
+          <a href='http://finance.yahoo.com/news/Top-Reason-Users-Dont-Click-iw-129671171.html?x=0&amp;.v=1' target='_blank'>
+            Average industry
+            <br />
+            click through rate of
+            <br />
+            less than 0.1%!
+          </a>
+        </td>
+        <td>
+          <div class='title_secondary text_blue_green'>SEO needs content</div>
+          <a href='http://searchenginewatch.com/article/2190655/Brands-as-Publishers-The-Changing-Rules-of-Search-SEO' target='_blank'>
+            No longer about
+            <br />
+            technical tricks, but
+            <br />
+            instead about quality
+            <br />
+            content
+          </a>
+        </td>
+        <td>
+          <div class='title_secondary text_blue_green'>Social needs content</div>
+          <a href='http://www.clickz.com/clickz/column/2178369/stages-social-media-marketing' target='_blank'>
+            Marketers must
+            <br />
+            distribute quality
+            <br />
+            content through
+            <br />
+            social channels
+          </a>
+        </td>
+      </table>
+      <div class='subtitle_1'>
+        LEAD NURTURING -
+      </div>
+      <div class='title_secondary text_blue_green margin_55l'>Content is the fuel that powers the Marketing Automation engine</div>
+      <a class='margin_55l margin_18t margin_34b block' href='http://www.marketingprofs.com/articles/2012/8445/content-fuel-for-the-marketing-automation-engine' target='_blank'>Email &amp; lead nurturing campaigns struggle without compelling content</a>
+      <div class='subtitle_1'>
+        BOTTOM LINE -
+      </div>
+      <div class='title_secondary text_blue_green stripe'>Content Marketing delivers superior lead results vs. all other tactics</div>
+      <table class='table_2'>
+        <td>
+          <a href='http://blog.hubspot.com/blog/tabid/6307/bid/10172/Inbound-Leads-Cost-62-Less-than-Outbound-New-Data.aspx' target='_blank'>
+            Inbound Leads Cost 62% Than
+            <br />
+            Outbound Leads
+          </a>
+        </td>
+        <td>
+          <a href='http://kapost.com/ebook_content_marketing_roi/' target='_blank'>
+            Content Marketing Leads Cost 41%
+            <br />
+            Less Than The Best Alternative
+            <br />
+            Channel, Paid Search
+          </a>
+        </td>
+      </table>
+      <img class='cloud' src='images/simple-cloud-3-sm.png' style='left: -35px; top: 760px' />
+      <img class='cloud' src='images/simple-cloud-2-sm.png' style='left: 400px; top: 815px' />
+      <img class='cloud zindex_9' src='images/complex-cloud-1-lg.png' style='left: -45px; top: 1015px' />
     </div>
   </div>
-  <div class='slide_with_bleed green_aqua'>
-    <div class='slide slide_short' id='slide_4'>
-      <div class='callout_awesome title_callout text_white'>If you want to emphasize a line or two of text, let's have an awesome callout</div>
+  <div class='slide slide_with_bleed' id='slide_4'>
+    <div class='slide_container blue_seafoam height_70'></div>
+    <div class='green_aqua zindex_10 relative'>
+      <div class='callout_awesome title_callout text_white'>
+        BUT MOST MARKETERS STRUGGLE WITH
+        <br />
+        CONTENT MARKETING
+      </div>
+      All Marketers know that they need to become publishers, but few are doing it well.
     </div>
   </div>
   <div class='slide blue_sky' id='slide_5'>
